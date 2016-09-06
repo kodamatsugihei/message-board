020fab02910e75d1f602aedb38eecf333cabfb8d
児玉継兵 <javajava552000@yahoo.co.jp>
2016-09-06 23:15:36 +0000

    challenge2 challenge submission

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,6 @@ class Message < ActiveRecord::Base
     validates :name , length: {maximum: 20}, presence: true
     # 内容は必須入力かつ２文字以上３０文字以下
     validates :body , length: {minimum: 2, maximum: 30}, presence: true
+    # 年齢が０以上の数字
+    validates :age , numericality: {only_integer: true, greater_than_or_equal_to: 0}
 end
